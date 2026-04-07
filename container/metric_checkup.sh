a1fb893866c6423ed894b853f47f7a42dcf53e6e
Sadanand Miskin <miskinsadanand@gmail.com>
2026-04-07 21:11:09 +0530

    fixed docker-compose + service + script file

--- container/metric_checkup.sh
+++ container/metric_checkup.sh
@@ -4,7 +4,7 @@
 THRESHOLD=80
 ALERT_LOG="/tmp/server_alert_report.log"
 SELF_LOG="/tmp/metric_log.log"
-ENDPOINT="http://10.233.9.84:5000/alert"
+ENDPOINT="http://192.168.1.42:5000/alert"
 OUTPUT=""
 
 # Ensure the log file is empty at the start
@@ -15,8 +15,8 @@ fetch_cpu_usage() {
     local cpu_snapshot=$(top -bn1)
     local cpu_idle=$(echo "$cpu_snapshot" | grep "Cpu(s)" | awk -F',' '{print $4}' | awk '{print $1}' | cut -d'.' -f1)
     local cpu_usage=$((100 - cpu_idle))
-    OUTPUT="${OUTPUT} Date: $(date) , CPU: ${cpu_usage}% "  
-    
+    OUTPUT="${OUTPUT} Date: $(date) , CPU: ${cpu_usage}% "
+
     if [ "$cpu_usage" -le $THRESHOLD ]; then
         now=$(date)
         echo -e "\n Reported On the Date: ${now} " >> "$ALERT_LOG"
@@ -30,8 +30,8 @@ fetch_mem_usage() {
     local mem_total=$(free -m | awk 'NR==2{print $2}')
     local mem_used=$(free -m | awk 'NR==2{print $3}')
     local used_per=$((mem_used * 100 / mem_total))
-    OUTPUT="${OUTPUT}  , Memory Used: ${used_per}% "   
-    
+    OUTPUT="${OUTPUT}  , Memory Used: ${used_per}% "
+
     if [ "$used_per" -le $THRESHOLD ]; then
         echo -e "\n[!] ALERT: HIGH MEMORY USAGE (${used_per}%)\n" >> "$ALERT_LOG"
         free -m >> "$ALERT_LOG"
@@ -45,8 +45,8 @@ fetch_storage_usage() {
     # Check root partition usage
     local storage_snapshot=$(df -h /)
     local percent=$(echo "$storage_snapshot" | awk 'NR==2 {print $5}' | sed 's/%//')
-    OUTPUT="${OUTPUT}  , Storage Used: ${percent}% "  
-    
+    OUTPUT="${OUTPUT}  , Storage Used: ${percent}% "
+
     if [ "$percent" -le $THRESHOLD ]; then
         echo -e "\n[!] ALERT: HIGH STORAGE USAGE (${percent}%)\n" >> "$ALERT_LOG"
         df -h >> "$ALERT_LOG"
